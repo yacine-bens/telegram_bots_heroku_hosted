bde055cb43f249a943e1274233bbc1af582d566b
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-10-11 00:08:53 +0100

    Added: PDF format + Desktop/Mobile devices

--- routes/puppeteer_screenshot_route.js
+++ routes/puppeteer_screenshot_route.js
@@ -28,6 +28,80 @@ router.get('/setWebhook', async (req, res) => {
 const { DB_URI } = process.env;
 const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const settings = {
+    device: {
+        values: {
+            android: 'Mozilla/5.0 (Linux; Android 13; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.79 Mobile Safari/537.36',
+            windows: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+            ios: 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/106.0.5249.92 Mobile/15E148 Safari/604.1',
+            mac: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+        },
+        keyboard: {
+            text: 'Choose Device',
+            inlineKeyboard: [
+                [{ text: 'Windows', callback_data: 'device_windows' }, { text: 'Mac', callback_data: 'device_mac' }],
+                [{ text: 'Android', callback_data: 'device_android' }, { text: 'iOS', callback_data: 'device_ios' }],
+                [{ text: 'Back to menu', callback_data: 'menu' }]
+            ]
+        }
+    },
+    resolution: {
+        values: {
+            '800': { width: 800, height: 600 },
+            '1280': { width: 1280, height: 800 },
+            '1440': { width: 1440, height: 900 },
+            '1920': { width: 1920, height: 1080 }
+        },
+        keyboard: {
+            text: 'Set Resolution',
+            inlineKeyboard: [
+                [{ text: '800x600', callback_data: 'resolution_800' }, { text: '1280x800', callback_data: 'resolution_1280' }],
+                [{ text: '1440x900', callback_data: 'resolution_1440' }, { text: '1920x1080', callback_data: 'resolution_1920' }],
+                [{ text: 'Back to menu', callback_data: 'menu' }]
+            ]
+        }
+    },
+    fullPage: {
+        values: {
+            'yes': true,
+            'no': false
+        },
+        keyboard: {
+            text: 'Capture Full Page ?',
+            inlineKeyboard: [
+                [{ text: 'Yes', callback_data: 'fullPage_yes' }, { text: 'No', callback_data: 'fullPage_no' }],
+                [{ text: 'Back to menu', callback_data: 'menu' }]
+            ]
+        }
+    },
+    format: {
+        values: {
+            'image': 'image',
+            'pdf': 'pdf'
+        },
+        keyboard: {
+            text: 'Choose Format',
+            inlineKeyboard: [
+                [{ text: 'Image', callback_data: 'format_image' }, { text: 'PDF', callback_data: 'format_pdf' }],
+                [{ text: 'Back to menu', callback_data: 'menu' }]
+            ]
+        }
+    },
+    menu: {
+        values: {},
+        keyboard: {
+            text: 'Screenshot settings',
+            inlineKeyboard: [
+                [{ text: 'Full Page', callback_data: 'fullPage' }],
+                [{ text: 'Resolution', callback_data: 'resolution' }],
+                [{ text: 'Device', callback_data: 'device' }],
+                [{ text: 'Format', callback_data: 'format' }],
+                [{ text: 'Exit', callback_data: 'exit' }]
+            ]
+        }
+    }
+};
+
 // Receive messages
 router.post(URI, async (req, res) => {
     console.log(JSON.stringify(req.body, null, 2));
@@ -55,32 +129,19 @@ router.post(URI, async (req, res) => {
                 await sendInlineKeyboard(chatId, msgId, cbQueryId, keyboard);
             }
         }
+        // Value queries (fullPage_no , resolution_800...)
         else {
-            // Value queries (fullPage_no , resolution_800)
             // Set settings
             const settingsName = cbQueryData.split('_')[0];
-            const settingsValue = cbQueryData.split('_')[1];
+            const settingsKey = cbQueryData.split('_')[1];
+            const settingsValue = settings[settingsName]['values'][settingsKey];
 
-            const settings = {
+            const newSettings = {
                 name: settingsName,
-                value: ''
+                value: settingsValue
             }
 
-            switch (settingsName) {
-                case 'fullPage':
-                    settings.value = settingsValue === 'yes';
-                    break;
-                case 'resolution':
-                    settings.value = {
-                        width: parseInt(settingsValue.split('x')[0]),
-                        height: parseInt(settingsValue.split('x')[1])
-                    }
-                    break;
-                default:
-                    break;
-            }
-
-            await setUserSettings(chatId, settings, db);
+            await setUserSettings(chatId, newSettings, db);
 
             await answerCallback(cbQueryId, 'Settings successfully changed.', true);
 
@@ -136,7 +197,7 @@ router.post(URI, async (req, res) => {
 
         //Respond to user
         if (screenshot != '') {
-            await sendPhoto(chatId, screenshot);
+            await sendPhoto(chatId, screenshot, userSettings.format);
         }
         else {
             await sendMessage(chatId, msg);
@@ -167,6 +228,8 @@ function isBotCommand(msg) {
 }
 
 async function takeScreenshot(url, settings) {
+    let screenshotBase64 = '';
+
     const options = {
         args: ['--no-sandbox', '--hide-scrollbars', '--disable-web-security', '--disable-setuid-sandbox'],
         defaultViewport: null,
@@ -180,13 +243,22 @@ async function takeScreenshot(url, settings) {
         await page.setViewport({
             ...settings.resolution,
             deviceScaleFactor: 1
-        })
+        });
+        await page.setUserAgent(settings.device);
         await page.goto(url, { waitUntil: 'networkidle2' });
-        const screenshotBase64 = await page.screenshot({
-            type: 'png',
-            fullPage: settings.fullPage,
-            encoding: 'base64'
-        })
+        if (settings.format === 'image') {
+            screenshotBase64 = await page.screenshot({
+                type: 'png',
+                fullPage: settings.fullPage,
+                encoding: 'base64'
+            })
+        }
+        else {
+            screenshotBase64 = await page.pdf({
+                ...settings.resolution,
+                printBackground: true
+            })
+        }
 
         await browser.close();
 
@@ -198,12 +270,13 @@ async function takeScreenshot(url, settings) {
     }
 }
 
-async function sendPhoto(chat_id, photo_base64) {
-    const buffer = Buffer.from(photo_base64, 'base64');
+async function sendPhoto(chat_id, screenshot_base64, format) {
+    const extension = format === 'image' ? '.png' : '.pdf';
+    const buffer = Buffer.from(screenshot_base64, 'base64');
     const formData = new FormData();
     formData.append('chat_id', chat_id);
-    // must specify filename (photo.png)
-    formData.append('document', buffer, 'photo.png');
+    // must specify filename (screenshot.png / screenshot.pdf)
+    formData.append('document', buffer, 'screenshot' + extension);
 
     await axios.post(`${TELEGRAM_API}/sendDocument`, formData, {
         headers: formData.getHeaders()
@@ -240,8 +313,10 @@ async function isRepeatedUpdate(chat_id, update_id, db) {
 
 async function getUserSettings(chat_id, db) {
     let userSettings = {
-        fullPage: false,
-        resolution: { width: 800, height: 600 }
+        fullPage: settings.fullPage.values['no'],
+        resolution: settings.resolution.values['800'],
+        device: settings.device.values['windows'],
+        format: settings.format.values['image']
     };
 
     const userSettingsCollection = db.collection('user_settings');
@@ -254,6 +329,8 @@ async function getUserSettings(chat_id, db) {
     else {
         userSettings.fullPage = result.fullPage;
         userSettings.resolution = result.resolution;
+        userSettings.device = result.device;
+        userSettings.format = result.format;
     }
 
     return userSettings;
@@ -269,34 +346,8 @@ function inlineKeyboard(cb_query_data) {
         text: '',
         inlineKeyboard: []
     }
-    switch (cb_query_data) {
-        case 'menu':
-            keyboard.text = 'Screenshot settings';
-            keyboard.inlineKeyboard = [
-                [{ text: 'Full Page', callback_data: 'fullpage' }],
-                [{ text: 'Resolution', callback_data: 'resolution' }],
-                [{ text: 'Exit', callback_data: 'exit' }]
-            ]
-            break;
-
-        case 'fullpage':
-            keyboard.text = 'Capture Full Page ?';
-            keyboard.inlineKeyboard = [
-                [{ text: 'Yes', callback_data: 'fullPage_yes' }, { text: 'No', callback_data: 'fullPage_no' }],
-                [{ text: 'Back to menu', callback_data: 'menu' }]
-            ]
-            break;
-        case 'resolution':
-            keyboard.text = 'Set Resolution';
-            keyboard.inlineKeyboard = [
-                [{ text: '800x600', callback_data: 'resolution_800x600' }, { text: '1280x800', callback_data: 'resolution_1280x800' }],
-                [{ text: '1440x900', callback_data: 'resolution_1440x900' }, { text: '1920x1080', callback_data: 'resolution_1920x1080' }],
-                [{ text: 'Back to menu', callback_data: 'menu' }]
-            ]
-            break;
-        default:
-            break;
-    }
+    keyboard.text = settings[cb_query_data]['keyboard']['text'];
+    keyboard.inlineKeyboard = settings[cb_query_data]['keyboard']['inlineKeyboard'];
 
     return keyboard;
 }
@@ -326,13 +377,9 @@ async function deleteMessage(chat_id, msg_id) {
 async function sendSettingsMenu(chat_id) {
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: chat_id,
-        text: 'Screenshot settings',
+        text: settings['menu']['keyboard']['text'],
         reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Full Page', callback_data: 'fullpage' }],
-                [{ text: 'Resolution', callback_data: 'resolution' }],
-                [{ text: 'Exit', callback_data: 'exit'}]
-            ]
+            inline_keyboard: settings['menu']['keyboard']['inlineKeyboard']
         }
     })
 }
